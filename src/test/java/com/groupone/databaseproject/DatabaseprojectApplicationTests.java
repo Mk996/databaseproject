75c52fffc3080b3a1c0e30adbae0521dc19223cc
Maharshi Gor <maharshigor@Maharshi-Gor.local>
2019-01-15 20:02:54 +0530

    Initial Commit

---
+++ src/test/java/com/groupone/databaseproject/DatabaseprojectApplicationTests.java
@@ -0,0 +1,17 @@
+package com.groupone.databaseproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DatabaseprojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
