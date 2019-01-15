2aca7a936f28650acb681226b01bccb281f7190f
Suchhanda Das <suchhanda@MacBook-Pro-101.local>
2019-01-16 00:04:14 +0530

    created SEMESTER classes across all packages

---
+++ src/main/java/com/groupone/databaseproject/services/impl/SemesterServicesImpl.java
@@ -0,0 +1,8 @@
+package com.groupone.databaseproject.services.impl;
+
+import com.groupone.databaseproject.services.SemesterServices;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SemesterServicesImpl implements SemesterServices {
+}
