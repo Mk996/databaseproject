39876faf210dc137bf51326aa2f8c906d64f3bbf
NagaSruthi Kota <nagasruthikota@NagaSruthis-MacBook-Pro.local>
2019-01-16 09:40:39 +0530

    pulling data till now

--- src/main/java/com/groupone/databaseproject/services/StudentServices.java
+++ src/main/java/com/groupone/databaseproject/services/StudentServices.java
@@ -1,4 +1,8 @@
 package com.groupone.databaseproject.services;
 
+import com.groupone.databaseproject.entity.Student;
+
 public interface StudentServices {
+
+    Student add(Student student);
 }
--- src/main/java/com/groupone/databaseproject/services/impl/StudentServicesImpl.java
+++ src/main/java/com/groupone/databaseproject/services/impl/StudentServicesImpl.java
@@ -1,8 +1,19 @@
 package com.groupone.databaseproject.services.impl;
 
+import com.groupone.databaseproject.entity.Student;
+import com.groupone.databaseproject.repository.StudentRepository;
 import com.groupone.databaseproject.services.StudentServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentServicesImpl implements StudentServices {
+
+    @Autowired
+    StudentRepository studentRepository
+
+    @Override
+    public Student add(Student student) {
+        studentRepository.save(student);
+    }
 }
