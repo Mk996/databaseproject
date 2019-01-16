d6bcbcc615ab68bba88549b72db36b2c910ad644
NagaSruthi Kota <nagasruthikota@NagaSruthis-MacBook-Pro.local>
2019-01-16 10:08:22 +0530

    commit by sruthi

--- src/main/java/com/groupone/databaseproject/services/StudentServices.java
+++ src/main/java/com/groupone/databaseproject/services/StudentServices.java
@@ -5,4 +5,7 @@ import com.groupone.databaseproject.entity.Student;
 public interface StudentServices {
 
     Student add(Student student);
+    Student select(String studentId);
+    Student update(Student student);
+    void delete(String studentId);
 }
--- src/main/java/com/groupone/databaseproject/services/impl/StudentServicesImpl.java
+++ src/main/java/com/groupone/databaseproject/services/impl/StudentServicesImpl.java
@@ -10,10 +10,28 @@ import org.springframework.stereotype.Service;
 public class StudentServicesImpl implements StudentServices {
 
     @Autowired
-    StudentRepository studentRepository
+    StudentRepository studentRepository;
 
     @Override
     public Student add(Student student) {
-        studentRepository.save(student);
+        Student student1 = studentRepository.save(student);
+        return student1;
+    }
+
+    @Override
+    public Student select(String studentId) {
+        Student student = studentRepository.findOne(studentId);
+        return student;
+    }
+
+    @Override
+    public Student update(Student student) {
+        Student student1 = studentRepository.save(student);
+        return student1;
+    }
+
+    @Override
+    public void delete(String studentId) {
+        studentRepository.delete(studentId);
     }
 }
