014139b076c6cb63c6f3e147e0168cbaf31c77e7
Maharshi Gor <maharshigor@Maharshi-Gor.local>
2019-01-16 11:35:24 +0530

    My final commit

--- src/main/java/com/groupone/databaseproject/controller/ProfessorController.java
+++ src/main/java/com/groupone/databaseproject/controller/ProfessorController.java
@@ -1,7 +1,7 @@
 package com.groupone.databaseproject.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.groupone.databaseproject.dto.ProfessorDTO;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by maharshigor on 15/01/19
@@ -10,6 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/professor")
 public class ProfessorController {
 
-    //I am going to implement some methods based on the agendas
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public void add(@RequestBody ProfessorDTO professorDTO){
+
+    }
+
+    @RequestMapping(value = "/select", method = RequestMethod.GET)
+    public ProfessorDTO select(@RequestParam String professorIdDTO){
+        return null;
+    }
+
+    @RequestMapping(value = "/update" , method = RequestMethod.PUT)
+    public void update(@RequestBody ProfessorDTO professorDTO){
+
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    public void delete(String professorIdDTO){
+
+    }
 
 }
