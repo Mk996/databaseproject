777289d249c667bc4f4d80ea1de031bccf8183c5
Suchhanda Das <suchhanda@MacBook-Pro-101.local>
2019-01-16 11:36:39 +0530

    Added subject contoller and services

--- src/main/java/com/groupone/databaseproject/controller/SubjectController.java
+++ src/main/java/com/groupone/databaseproject/controller/SubjectController.java
@@ -1,11 +1,67 @@
 package com.groupone.databaseproject.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.groupone.databaseproject.dto.SubjectDTO;
+import com.groupone.databaseproject.entity.Department;
+import com.groupone.databaseproject.entity.Semester;
+import com.groupone.databaseproject.entity.Subject;
+import com.groupone.databaseproject.services.SemesterServices;
+import com.groupone.databaseproject.services.SubjectServices;
+import com.groupone.databaseproject.services.impl.DepartmentServicesImpl;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping(value = "/subject")
 public class SubjectController {
+
+    @Autowired
+    private SubjectServices subjectServices;
+
+    @Autowired
+    private SemesterServices semesterServices;
+
+    @Autowired
+    private DepartmentServicesImpl departmentServices;
+
+    @PostMapping("/add")
+    private SubjectDTO add(@RequestBody SubjectDTO subject) {
+        Subject newSubject = new Subject();
+        BeanUtils.copyProperties(subject, newSubject);
+        Semester semester = semesterServices.select(subject.getSemesterId());
+        Department department = departmentServices.select(subject.getDepartmentId());
+        newSubject.setSemester(semester);
+        newSubject.setDepartment(department);
+        Subject subCreated = subjectServices.add(newSubject);
+        SubjectDTO newSubjectDto = new SubjectDTO();
+        BeanUtils.copyProperties(subCreated, newSubjectDto);
+        newSubjectDto.setSemesterId(subCreated.getSemester().getSemesterID());
+        newSubjectDto.setDepartmentId(subCreated.getDepartment().getDepartmentId());
+        return newSubjectDto;
+    }
+
+    @GetMapping("/select")
+    private SubjectDTO select(@RequestParam String id) {
+        Subject subject = subjectServices.select(id);
+        SubjectDTO newSubjectDto = new SubjectDTO();
+        BeanUtils.copyProperties(subject, newSubjectDto);
+        return newSubjectDto;
+    }
+
+    @PutMapping("/update")
+    private SubjectDTO update(@RequestBody SubjectDTO subject) {
+        Subject newSubject = new Subject();
+        BeanUtils.copyProperties(subject, newSubject);
+        Subject subCreated = subjectServices.update(newSubject);
+        SubjectDTO newSubjectDto = new SubjectDTO();
+        BeanUtils.copyProperties(subCreated, newSubjectDto);
+        return newSubjectDto;
+    }
+
+    @DeleteMapping("/delete")
+    private void delete(@RequestParam String id){
+        subjectServices.delete(id);
+    }
 }
--- src/main/java/com/groupone/databaseproject/services/SubjectServices.java
+++ src/main/java/com/groupone/databaseproject/services/SubjectServices.java
@@ -1,5 +1,15 @@
 package com.groupone.databaseproject.services;
 
+import com.groupone.databaseproject.dto.SubjectDTO;
+import com.groupone.databaseproject.entity.Subject;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface SubjectServices {
+    Subject add(Subject subject);
+    Subject select(String id);
+    Subject update(Subject subject);
+    void delete(String id);
+
+
 }
